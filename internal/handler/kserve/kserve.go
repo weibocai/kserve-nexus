2e49407dfa1c5bf74758e373e36a7b42912d4415
weicai <37139966+weibocai@users.noreply.github.com>
2026-07-08 09:35:21 +0800

    更新 kserve.go

--- internal/handler/kserve/kserve.go
+++ internal/handler/kserve/kserve.go
@@ -249,6 +249,6 @@ func (kh *Handler) GetIsvc(c *gin.Context) {
 	if dm == "Knative" {
 		nodes = KnativeIsvc2GraphNode(c, kh.kc, &isvc, ingressConfig)
 	}
-	response["nodes"], response["edges"] = nodes.GetNodes(), nodes.GetEdges()
+	response["graph"] = nodes
 	middleware.SuccessJson(c, response)
 }
